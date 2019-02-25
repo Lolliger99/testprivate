889630ba1a3e5a6116cb81af336ad37ab0bded91
Thies <loligerer99@gmail.com>
2019-02-25 17:38:10 +0100

    AllNodesConnected improved

--- testprivate/src/testprivate/test.java
+++ testprivate/src/testprivate/test.java
@@ -2,5 +2,9 @@ package testprivate;
 
 public class test {
 
-
+	public static int random;
+	public static void main (String[] args) {
+		random = (int) Math.random();
+		System.out.println(random);
+	}
 }
